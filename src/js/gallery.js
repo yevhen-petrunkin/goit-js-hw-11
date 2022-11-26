d4b4efb1ea5160b2cbdeb6b3ca68289bd5694c98
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-27 00:48:23 +0200

    Finish main functionality

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api';
 const searchParams = new URLSearchParams({
   key: '31610284-d2a9adb661769c66f83a1d1f1',
@@ -10,6 +11,7 @@ const searchParams = new URLSearchParams({
 });
 
 let pageCounter = 1;
+let totalHits = 0;
 
 const refs = {
   searchFormRef: document.querySelector('#search-form'),
@@ -22,26 +24,42 @@ refs.loadMoreBtnRef.addEventListener('click', onLoadMore);
 
 function onSearchFormSubmit(evt) {
   evt.preventDefault();
+  resetGallery();
+  resetPageCounter();
   const query = evt.currentTarget.elements.searchQuery.value.trim();
   if (query === '') {
     return;
   }
   setSearchQueryValue(query);
-  fetchSearchQuery();
+  fetchSearchQuery().then(data => {
+    setTotalHits(data);
+    createCardMarkup(data);
+  });
 }
 
 function setSearchQueryValue(query) {
   searchParams.set('q', query);
 }
 
-function fetchSearchQuery() {
-  fetch(`${BASE_URL}/?${searchParams}`)
-    .then(r => r.json())
-    .then(createCardMarkup);
+async function fetchSearchQuery() {
+  try {
+    const response = await axios.get(`${BASE_URL}/?${searchParams}`);
+    const data = await response.data;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
 }
 
 function createCardMarkup(data) {
   const array = data.hits;
+  if (array.length === 0) {
+    console.log(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
   array.forEach(obj => {
     const markup = `<div class="gallery__item photo-card"><a class="gallery__link" href="${obj.largeImageURL}"><img class="gallery__image" src="${obj.webformatURL}" alt="${obj.tags}" loading="lazy" /><div class="info"><p class="info__item"><b>Likes</b><span class="info__data">${obj.likes}</span></p><p class="info__item"><b>Views</b><span class="info__data">${obj.views}</span></p><p class="info__item"><b>Comments</b><span class="info__data">${obj.comments}</span></p><p class="info__item"><b>Downloads</b><span class="info__data">${obj.downloads}</span></p></div></a></div>`;
     refs.galleryRef.insertAdjacentHTML('beforeend', markup);
@@ -49,13 +67,42 @@ function createCardMarkup(data) {
 }
 
 function onLoadMore() {
-  pageCounter += 1;
+  countTotalHits();
+  if (totalHits <= 0) {
+    console.log("We're sorry, but you've reached the end of search results.");
+    return;
+  }
+  countPage();
   setPageValue(pageCounter);
-  fetchSearchQuery();
+  fetchSearchQuery().then(createCardMarkup);
 }
 
 function setPageValue(number) {
   searchParams.set('page', number);
+}
+
+function getPerPageValue() {
+  return searchParams.get('per_page');
+}
+
+function resetGallery() {
+  refs.galleryRef.innerHTML = '';
+}
+function resetPageCounter() {
+  pageCounter = 1;
+}
+
+function setTotalHits(data) {
+  totalHits = data.totalHits;
+}
+
+function countPage() {
+  pageCounter += 1;
+}
+
+function countTotalHits() {
+  totalHits -= getPerPageValue();
+  console.log(totalHits);
 }
 
 /* <div class="gallery__item photo-card">
