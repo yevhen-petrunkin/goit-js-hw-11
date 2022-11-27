cb25ccc5c0a4adbccf427c3b6997b94d32539ef2
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-27 03:48:34 +0200

    Add simplelightbox

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const BASE_URL = 'https://pixabay.com/api';
 const FAIL_MESSAGE =
@@ -17,6 +19,7 @@ const searchParams = new URLSearchParams({
   per_page: 40,
 });
 
+let markup = '';
 let pageCounter = 1;
 let totalHits = 0;
 
@@ -41,6 +44,7 @@ function onSearchFormSubmit(evt) {
   fetchSearchQuery().then(data => {
     setTotalHits(data);
     createCardMarkup(data);
+    enableLargeImagesInGallery();
   });
 }
 
@@ -51,8 +55,7 @@ function setSearchQueryValue(query) {
 async function fetchSearchQuery() {
   try {
     const response = await axios.get(`${BASE_URL}/?${searchParams}`);
-    const data = await response.data;
-    return data;
+    return await response.data;
   } catch (error) {
     console.log('Error:', error.message);
   }
@@ -64,18 +67,30 @@ function createCardMarkup(data) {
     notifyOnFail();
     return;
   }
-  drawMarkups(array);
+  drawMarkup(array);
+  appendMarkupToGallery(markup);
 }
 
-function drawMarkups(array) {
-  array.forEach(obj => {
-    const markup = `<div class="gallery__item photo-card"><a class="gallery__link" href="${obj.largeImageURL}"><img class="gallery__image" src="${obj.webformatURL}" alt="${obj.tags}" loading="lazy" /><div class="info"><p class="info__item"><b>Likes</b><span class="info__data">${obj.likes}</span></p><p class="info__item"><b>Views</b><span class="info__data">${obj.views}</span></p><p class="info__item"><b>Comments</b><span class="info__data">${obj.comments}</span></p><p class="info__item"><b>Downloads</b><span class="info__data">${obj.downloads}</span></p></div></a></div>`;
-    appendMarkupToGallery(markup);
-  });
+function drawMarkup(array) {
+  return (markup = array
+    .map(obj => {
+      return `<a class="gallery__link" href="${obj.largeImageURL}"><img class="gallery__image" src="${obj.webformatURL}" alt="${obj.tags}" loading="lazy" /><div class="info"><p class="info__item"><b>Likes</b><span class="info__data">${obj.likes}</span></p><p class="info__item"><b>Views</b><span class="info__data">${obj.views}</span></p><p class="info__item"><b>Comments</b><span class="info__data">${obj.comments}</span></p><p class="info__item"><b>Downloads</b><span class="info__data">${obj.downloads}</span></p></div></a>`;
+    })
+    .join(''));
 }
 
 function appendMarkupToGallery(markup) {
   refs.galleryRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function enableLargeImagesInGallery() {
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function (evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+      return;
+    }
+  });
 }
 
 function notifyOnFail() {
